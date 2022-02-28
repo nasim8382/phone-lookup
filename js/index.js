1bc5a2053c43814e25aeb0bbf4d59641fe5f9e40
Nasim Reza <nasim8382@gmail.com>
2022-03-01 00:23:58 +0600

    search field error handeling added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('testing');
+const searchButton = () => {
+    const input = document.getElementById('input-value');
+    const error = document.getElementById('error-message');
+    const inputValue = input.value;
+    const samsung = 'samsung';
+    const iphone = 'iphone';
+    const oppo = 'oppo';
+    const huawei = 'huawei';
+    if (inputValue.toLowerCase() === samsung || inputValue.toLowerCase() === iphone || inputValue.toLowerCase() === oppo || inputValue.toLowerCase() === huawei) {
+        error.innerText = 'Phone found';
+        input.value = '';
+    }
+    else if(inputValue == ''){
+        error.innerText = 'Please input a phone name !!!';
+        input.value = '';
+    }
+    else if (isNaN(inputValue) == false && parseInt(inputValue) !== 'string'){
+        error.innerText = 'Please avoid a number input !!!';
+        input.value = '';
+    }
+    else{
+        error.innerText = 'Phone not found, please give a valid input !!!';
+    }
+}
